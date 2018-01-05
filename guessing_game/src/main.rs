07373fb06e7c45e60854c0a36e31641687755de8
otofu-square <yukita@feedforce.jp>
2018-01-05 22:21:48 +0900

    :package: Add Cargo.lock

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,14 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Guess the 数!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -9,5 +16,5 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line.");
 
-    println!("You guessed {}", guess);
+    println!("You guessed: {}", guess);
 }
