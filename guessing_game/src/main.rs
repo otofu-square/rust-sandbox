094a9443d7c2aa0bbfe1b1d2afca470e37a889e6
otofu-square <yukita@feedforce.jp>
2018-01-06 22:24:11 +0900

    :+1: Add ordering pattern match

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -16,5 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line.");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number.");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("Same!"),
+    };
 }
