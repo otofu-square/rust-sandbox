817eec9ac5c6f938634fb50b897f1a2f12e53968
otofu-square <yukita@feedforce.jp>
2018-01-08 20:30:33 +0900

    :wrench: Add left & right

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -1,18 +1,31 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::{Arc, Mutex};
+
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
 
 struct Philosopher {
     name: String,
+    left: usize,
+    right: usize,
 }
 
 impl Philosopher {
-    fn new(name: &str) -> Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
+            left: left,
+            right: right,
         }
     }
 
-    fn eat(&self) {
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        thread::sleep(Duration::from_millis(150));
+        let _right = table.forks[self.right].lock().unwrap();
+
         println!("{} is eating.", self.name);
         thread::sleep(Duration::from_millis(1000));
         println!("{} is done eating.", self.name);
@@ -20,19 +33,30 @@ impl Philosopher {
 }
 
 fn main() {
+    let table = Arc::new(Table {
+        forks: vec![
+            Mutex::new(()),
+            Mutex::new(()),
+            Mutex::new(()),
+            Mutex::new(()),
+            Mutex::new(()),
+        ],
+    });
+
     let philosophers = vec![
-        Philosopher::new("Judith Butler"),
-        Philosopher::new("Gilles Deleuze"),
-        Philosopher::new("Karl Marx"),
-        Philosopher::new("Emma Goldman"),
-        Philosopher::new("Michel Foucault"),
+        Philosopher::new("Judith Butler", 0, 1),
+        Philosopher::new("Gilles Deleuze", 1, 2),
+        Philosopher::new("Karl Marx", 2, 3),
+        Philosopher::new("Emma Goldman", 3, 4),
+        Philosopher::new("Michel Foucault", 0, 4),
     ];
 
     let handles: Vec<_> = philosophers
         .into_iter()
         .map(|p| {
+            let table = table.clone();
             thread::spawn(move || {
-                p.eat();
+                p.eat(&table);
             })
         })
         .collect();
