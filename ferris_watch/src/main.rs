55d3439ee029d60646a223de751a27a212b79229
otofu-square <otofu.square@gmail.com>
2018-12-13 20:53:07 +0900

    :wrench: implement loop handling

--- ferris_watch/src/main.rs
+++ ferris_watch/src/main.rs
@@ -1,35 +1,65 @@
 use clap::{value_t, App, Arg};
 use log::debug;
+use std::process::Command;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() -> Result<(), failure::Error> {
     env_logger::init();
     debug!("ferris_watch starting ...");
 
-    let matches = App::new("ferris_watch")
-        .version("0.1.0")
-        .author("otofu-square")
-        .about("cute watch command")
-        .arg(
-            Arg::with_name("command")
-                .required(true)
-                .multiple(true)
-                .help("The command to run periodically"),
-        )
-        .arg(
-            Arg::with_name("interval")
-                .long("interval")
-                .short("n")
-                .takes_value(true)
-                .default_value("2.0")
-                .help("The period to run a command"),
-        )
-        .get_matches();
+    'outer: loop {
+        let interrupted = Arc::new(AtomicBool::new(false));
+        signal_hook::flag::register(signal_hook::SIGINT, interrupted.clone())?;
+        let interrupted = || interrupted.load(Ordering::SeqCst);
 
-    let command = matches.values_of("command").unwrap().collect::<Vec<_>>();
-    let interval = value_t!(matches, "interval", f64)?;
+        let matches = App::new("ferris_watch")
+            .version("0.1.0")
+            .author("otofu-square")
+            .about("cute watch command")
+            .arg(
+                Arg::with_name("command")
+                    .required(true)
+                    .multiple(true)
+                    .help("The command to run periodically"),
+            )
+            .arg(
+                Arg::with_name("interval")
+                    .long("interval")
+                    .short("n")
+                    .takes_value(true)
+                    .default_value("2.0")
+                    .help("The period to run a command"),
+            )
+            .get_matches();
 
-    debug!("command = {:?}", command);
-    debug!("interval = {:?}", interval);
+        let command = matches.values_of("command").unwrap().collect::<Vec<_>>();
+        let interval = value_t!(matches, "interval", f64)?;
 
+        debug!("command = {:?}", command);
+        debug!("interval = {:?}", interval);
+
+        let output = Command::new(command[0]).args(&command[1..]).output()?;
+        let output = String::from_utf8_lossy(&output.stdout);
+        println!("{}", output);
+
+        let interval10 = (interval * 10.0) as u32;
+
+        // for に入る前に interrupted のフラグが立った場合
+        if interrupted() {
+            break 'outer;
+        }
+
+        for _ in 0..interval10 {
+            sleep(Duration::from_millis(100));
+            if interrupted() {
+                break 'outer;
+            }
+        }
+    }
+
+    debug!("end");
     Ok(())
 }
