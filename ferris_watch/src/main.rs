77b9385075b3f7f31704c6d57dd93423c4433082
otofu-square <otofu.square@gmail.com>
2018-12-13 20:35:31 +0900

    :wrench: implement interval & command args

--- ferris_watch/src/main.rs
+++ ferris_watch/src/main.rs
@@ -1,4 +1,4 @@
-use clap::App;
+use clap::{value_t, App, Arg};
 use log::debug;
 
 fn main() -> Result<(), failure::Error> {
@@ -9,7 +9,27 @@ fn main() -> Result<(), failure::Error> {
         .version("0.1.0")
         .author("otofu-square")
         .about("cute watch command")
+        .arg(
+            Arg::with_name("command")
+                .required(true)
+                .multiple(true)
+                .help("The command to run periodically"),
+        )
+        .arg(
+            Arg::with_name("interval")
+                .long("interval")
+                .short("n")
+                .takes_value(true)
+                .default_value("2.0")
+                .help("The period to run a command"),
+        )
         .get_matches();
+
+    let command = matches.values_of("command").unwrap().collect::<Vec<_>>();
+    let interval = value_t!(matches, "interval", f64)?;
+
+    debug!("command = {:?}", command);
+    debug!("interval = {:?}", interval);
 
     Ok(())
 }
