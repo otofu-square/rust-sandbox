74ac9c1c4586295ebf0d67e0566aa4c1b2203c19
otofu-square <otofu.square@gmail.com>
2018-12-13 18:26:07 +0900

    :wrench: implement guessing logic

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,13 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -12,4 +18,12 @@ fn main() {
         .expect("Failed to read line.");
 
     println!("You guessed: {}", guess);
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
