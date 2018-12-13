d9844fea08dd76610fa03ecf157b1c63de1899c0
otofu-square <otofu.square@gmail.com>
2018-12-13 20:22:35 +0900

    :wrench: implement --help option

--- ferris_watch/src/main.rs
+++ ferris_watch/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use clap::App;
+use log::debug;
+
+fn main() -> Result<(), failure::Error> {
+    env_logger::init();
+    debug!("ferris_watch starting ...");
+
+    let matches = App::new("ferris_watch")
+        .version("0.1.0")
+        .author("otofu-square")
+        .about("cute watch command")
+        .get_matches();
+
+    Ok(())
 }
